b0d98366c6446863e55cb9c9db9215369eeeb3fe
Peter Matula <peter.matula@avast.com>
2020-03-23 03:23:34 +0100

    progress

--- pelib/pelib-example.cpp
+++ pelib/pelib-example.cpp
@@ -1,8 +1,39 @@
 
 #include <iostream>
 
-int main()
+#include <retdec/pelib/PeFile.h>
+#include <retdec/pelib/PeLib.h>
+
+int main(int argc, char* argv[])
 {
-	std::cout << "hello world" << std::endl;
+	if (argc != 2)
+	{
+		std::cerr << "Error: Expecting one argument - path to PE file\n";
+		return 1;
+	}
+	std::string input = argv[1];
+
+	PeLib::PeFile* pe = PeLib::openPeFile(input);
+	if (pe)
+	{
+		try
+		{
+			pe->readMzHeader();
+
+			std::cout << "Parsing succeeded for '" << input << "'\n";
+			std::cout << "MZ header size = " << pe->mzHeader().size() << "\n";
+		}
+		catch (...)
+		{
+			std::cerr << "Error: parsing failed for '" << input << "'\n";
+			return 1;
+		}
+	}
+	else
+	{
+		std::cerr << "Error: parsing failed for '" << input << "'\n";
+		return 1;
+	}
+
 	return 0;
 }
